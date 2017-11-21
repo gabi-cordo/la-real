6e9a170846d85a811277c62164a9cdcac2234a99
gabi-cordo <portekslas@gmail.com>
2017-11-20 23:10:13 -0300

    ultima modificacion

---
+++ js/accordion.js
@@ -0,0 +1,26 @@
+$(function(){
+	
+	var Accordion = function(el, multiple){
+		this.el = el || {};
+		this.multiple = multiple || false;
+		
+		var link = this.el.find('.link');
+		
+		link.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+	Accordion.prototype.dropdown = function(e){
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+			
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+		
+		if (!e.data.multiple){
+			$el.find('.a').not($next).slideUp().parent().removeClass('open');
+			};
+		
+	}
+	var accordion = new Accordion($('#accordion'), false);
+	
+	});
